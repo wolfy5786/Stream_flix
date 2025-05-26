f5377620bf9e32884a5ace8be2a8d0180d555d7d
Wolf1824 <makhijaniankush24@gmail.com>
2025-05-26 17:02:22 -0400

    first commit

---
+++ src/test/java/com/streaming/streamflix/StreamflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.streaming.streamflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
